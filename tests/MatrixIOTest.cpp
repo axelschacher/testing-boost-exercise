77d9c2bc282b045edfec7e6e1a907d3c326bd0bc
schumaax <axel.schumacher@meisterfliesen.ws>
2022-02-02 09:24:45 +0100

    format style fixes

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -7,8 +7,8 @@ BOOST_AUTO_TEST_SUITE(MatrixIOTests)
 BOOST_AUTO_TEST_CASE(openData)
 {
   const std::string fileToOpen = "../data/m3.csv";
-  const int matrixSize{3};
-  Eigen::MatrixXd expectedMatrix(3, 3);
+  const int         matrixSize{3};
+  Eigen::MatrixXd   expectedMatrix(3, 3);
   expectedMatrix << 0.680375, 0.59688, -0.329554, -0.211234, 0.823295, 0.536459, 0.566198, -0.604897, -0.444451;
 
   Eigen::MatrixXd returned_matrix = matrixIO::openData(fileToOpen, matrixSize);
