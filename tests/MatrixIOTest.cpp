ce22c683a95aef667fe6a5767e90bf2bbe09ff76
schumaax <axel.schumacher@meisterfliesen.ws>
2022-02-02 09:08:19 +0100

    style fixes

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -7,11 +7,9 @@ BOOST_AUTO_TEST_SUITE(MatrixIOTests)
 BOOST_AUTO_TEST_CASE(openData)
 {
   const std::string fileToOpen = "../data/m3.csv";
-  const int                             matrixSize{3};
+  const int matrixSize{3};
   Eigen::MatrixXd expectedMatrix(3, 3);
-  expectedMatrix << 0.680375, 0.59688, -0.329554,
-                    -0.211234, 0.823295, 0.536459,
-                    0.566198, -0.604897, -0.444451;
+  expectedMatrix << 0.680375, 0.59688, -0.329554, -0.211234, 0.823295, 0.536459, 0.566198, -0.604897, -0.444451;
 
   Eigen::MatrixXd returned_matrix = matrixIO::openData(fileToOpen, matrixSize);
   BOOST_TEST(expectedMatrix == returned_matrix); // Should work without rounding errors because no additions/... made.
